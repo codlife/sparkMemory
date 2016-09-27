ee21586a9fd1685a96ff6221c8d7d429ceb5cbfe
codlife <1004910847@qq.com>
2016-09-27 22:40:39 +0800

    add read.md

---
+++ src/main/scala/org/apache/spark/sparkInternal/Frist.scala
@@ -0,0 +1,11 @@
+package scala.org.apache.spark.sparkInternal
+
+/**
+  * Created by wjf on 16-9-27.
+  */
+object Frist {
+  def main(args: Array[String]): Unit = {
+    println("hello spark")
+  }
+
+}
